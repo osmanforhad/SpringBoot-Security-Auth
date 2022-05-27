ae7da091dc5d6f414efacfa56b3d202f8ef39345
osmanforhad <developer.osmanforhad@gmail.com>
2022-05-27 16:40:28 +0600

    create Database Model Entity and setup Many to Many RelationShip

---
+++ src/main/java/net/osmanforhad/main/model/Role.java
@@ -0,0 +1,49 @@
+package net.osmanforhad.main.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "role")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	//no argument constructor
+	public Role() {
+		super();
+	}
+	
+	//all argument constructor
+	public Role(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	//getter and setter methods
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
