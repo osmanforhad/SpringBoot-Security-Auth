3f40999bcd09a81aa26112695a8249cdd81851c9
osmanforhad <developer.osmanforhad@gmail.com>
2022-05-28 11:41:53 +0600

    fix the Spring Security Configuration UserServiceDetails and allow circular error issues

--- src/main/java/net/osmanforhad/main/config/SecurityConfiguration.java
+++ src/main/java/net/osmanforhad/main/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package net.osmanforhad.main.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -18,6 +19,7 @@ import net.osmanforhad.main.service.UserService;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 	//inject UserService Interface as dependency
+	@Autowired
 	private UserService userService;
 	
 	@Bean
